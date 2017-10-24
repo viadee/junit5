084429af63a8d76fe9e3d790c5bbb34d81cfd6fb
tobiaslvoss <tobias.voss@viadee.de>
2017-10-24 23:25:29 +0200

    JUnit4 Legacy Test

--- src/test/java/de/viadee/junit4/JUnit4LegacyTest.java
+++ src/test/java/de/viadee/junit4/JUnit4LegacyTest.java
@@ -1,14 +1,43 @@
 package de.viadee.junit4;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.sql.SQLException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class JUnit4LegacyTest {
-
+	
+	private String firma;
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		// umfangreiche Initialisierung (DB-Verbindung, ...)
+	}
+	
+	@Before
+	public void setUp() {
+		firma = "viadee";
+	}
+	
+	@Test
+	public void testFirma() {
+		assertEquals("Firma != viadee", "viade", firma);
+	}
+	
+	@Ignore
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		// momentan nicht lauffähig
+	}
+	
+	@AfterClass
+	public static void tearDownClass() throws SQLException {
+		// Abbau der Ressourcen und Infrastruktur
 	}
 
 }
