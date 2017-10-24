34b9128e0dee3fdebf54e6c4c29d950fde580e73
tobiaslvoss <tobias.voss@viadee.de>
2017-10-24 07:45:54 +0200

    Architektur-Bild und Refactoring AssertAllTest

--- src/test/java/de/viadee/junit5/ZweiterTest.java
+++ src/test/java/de/viadee/junit5/ZweiterTest.java
@@ -1,12 +1,9 @@
 package de.viadee.junit5;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
@@ -33,37 +30,5 @@ public class ZweiterTest {
 		// kompletter Ende-zu-Ende-Testfall
 	}
 	
-	@Test
-	void testAssertAll() {
-		Autor autor = new Autor("Tobias", "Voß");
-		assertAll("autor",
-				() -> assertEquals("Tobi", autor.getVorname()),
-				() -> assertEquals("Voss", autor.getNachname())
-		);
-	}
-	
-	
-	class Autor {
-		String vorname;
-		String nachname;
-		
-		public Autor(String vorname, String nachname) {
-			super();
-			this.vorname = vorname;
-			this.nachname = nachname;
-		}
-		public String getVorname() {
-			return vorname;
-		}
-		public void setVorname(String vorname) {
-			this.vorname = vorname;
-		}
-		public String getNachname() {
-			return nachname;
-		}
-		public void setNachname(String nachname) {
-			this.nachname = nachname;
-		}
-		
-	}
+
 }
