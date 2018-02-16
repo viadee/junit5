ee30cac921f0ab79a36970c6a7e270165576c955
tobiaslvoss <tobias.voss@viadee.de>
2018-02-16 22:26:48 +0100

    added CsvFileSource example

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # JUnit5
-Code-Beispiele zum Vortrag "JUnit 5 - Testen neu definiert mit strukturierten, dynamischen und erweiterbaren Testfällen" auf der [JCON 2017](http://jcon.one/de/) in Düsseldorf
+Code-Beispiele zum Vortrag "JUnit 5 - Testen neu definiert mit strukturierten, dynamischen und erweiterbaren Testfällen" auf der [JCON 2017](http://jcon.one/de/) in Düsseldorf und auf dem [Frankfurter Entwicklertag 2018](https://entwicklertag.de/frankfurt/2018/junit-5-testen-neu-definiert-mit-strukturierten-dynamischen-und-erweiterbaren-testfällen)
--- src/test/java/de/viadee/junit5/ParametrisierterTest.java
+++ src/test/java/de/viadee/junit5/ParametrisierterTest.java
@@ -1,8 +1,10 @@
 package de.viadee.junit5;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.runner.JUnitPlatform;
@@ -13,9 +15,21 @@ public class ParametrisierterTest {
 	
 	@ParameterizedTest
 	@ValueSource(strings = {"Münster", "Köln"})
-	@CsvSource({"Münster, 800", "Köln, 40"})
-	public void testMitParametern(String param) {
-		assertNotNull(param);
+	public void testMitValueSource(String stadt) {
+		assertNotNull(stadt);
 	}
 
+	@ParameterizedTest
+	@CsvSource({"Münster, 80", "Köln, 50"})
+	public void testMitCsvSource(String stadt, int mitarbeiter) {
+		assertNotNull(stadt);
+		assertTrue(mitarbeiter > 40);
+	}
+
+	@ParameterizedTest(name = "{index} => stadt={0}, mitarbeiter={1}")
+	@CsvFileSource(resources="/parameters.csv")
+	public void testMitCsvFileSource(String stadt, int mitarbeiter) {
+		assertNotNull(stadt);
+		assertTrue(mitarbeiter > 0);
+	}
 }
